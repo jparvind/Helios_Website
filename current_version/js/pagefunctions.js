b3b12a0e9bfbc2fea92df9c2d567afdaf2af6e96
Joe <wirelesstaco@gmail.com>
2012-12-31 21:19:23 -0500

    Javacript for Solutions

--- current_version/js/pagefunctions.js
+++ current_version/js/pagefunctions.js
@@ -80,3 +80,84 @@ function current_nav_slide(){
 	    return false;
  	});
  }
+
+ function our_work_hide(){
+     //TODO crossfade
+        //Hide all our_work_projects exect first one
+        $("#our_workTop").children().not(".our_work_project:nth-Child(1)").hide();
+          //$("#our_workTop").children().not('#our_work_project02').hide();
+    
+
+        /*
+          $('#our_workTop').children().fadeOut("slow", function(){
+              $("#our_workTop").children().not('#our_work_project01').hide();
+              //$(this).replaceWith(div);
+              $('#our_work_project02').fadeIn("slow");
+          });
+          // $('#our_work').replaceWith('<h2>New heading</h2>');
+            */  
+            //Dim out all but first Thumb
+           $('div.our_work_thumbs').children().not(':first-Child').css({ opacity: 0.5 });
+          $(".our_work_thumbs > a ").click(function() {
+            
+            //Fade All but selected Thumb Out
+               $('div.our_work_thumbs').children().not($(this)).fadeTo('fast', 0.4);
+               //Fad Selected Thumb in
+              $(this).fadeTo('fast', 1);
+
+              //Change Project 
+              //Hide all but selected
+              var selectedIndex = $(this).index()+1;
+              //alert(selectedIndex);
+              //$("#our_workTop").children().not(1).hide();
+              //Swap Gallery Page
+              $("#our_workTop").children().not(".our_work_project:nth-Child("+selectedIndex+")").hide();
+              $(".our_work_project:nth-Child("+selectedIndex+")").show();
+            });
+ }
+function our_work_slider(){
+  
+    $(".touchslider-demo").touchSlider({
+      duration: 1000, // the speed of the sliding animation in milliseconds
+      delay: 5000, // initial auto-scrolling delay for each loop
+      margin: 5, // borders size. The margin is set in pixels.
+      mouseTouch: false,
+      namespace: "touchslider",
+      next: ".touchslider-next", // jQuery object for the elements to which a "scroll forwards" action should be bound.
+      pagination: ".touchslider-nav-item",
+      currentClass: "touchslider-nav-item-current", // class name for current pagination item.
+      prev: ".touchslider-prev", // jQuery object for the elements to which a "scroll backwards" action should be bound.
+
+      autoplay: true, // whether to move from image to image automatically
+      });
+      /*
+      $(function() {
+            $('#our_workThuwmbs').fadeTo('slow', 0.5, function() {
+       });
+      */
+
+}
+
+ function solutions_hide(){
+          //Hide not selected
+         $("#solutionDescContain").children().not(".solutionDesc:nth-Child(1)").hide();
+          //Thumbs 
+          $("#solutionButtonsContain > a ").click(function() {
+           
+            //Fade All but selected Thumb Out
+               $('#solutionButtonsContain').children().not($(this)).children().removeClass("solutionsButtonSelected");
+               //Fad Selected Thumb in
+              $(this).children(":first").addClass("solutionsButtonSelected");
+
+              //Change Project 
+              //Hide all but selected
+              var selectedIndex = $(this).index()+1;
+   
+              //Swap Content
+              $("#solutionDescContain").children().not(".solutionDesc:nth-Child("+selectedIndex+")").hide();
+              $(".solutionDesc:nth-Child("+selectedIndex+")").show();
+
+              //Swap background Image
+            //  alert($(#value).val('//'));
+            });
+ }
